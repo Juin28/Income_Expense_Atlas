b1e44aae16ecd4a0eade4f38d891bbefdd19f1cf
Juin Tan <juin@kth.se>
2025-02-10 22:38:13 +0100

    set up the skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
+import EarnAndSpendPage from './pages/EarnAndSpendPage'
+import CountryComparePage from './pages/CountryComparePage'
+import CountryStatisticPage from './pages/CountryStatisticPage'
+import Layout from './Layout'
 
 function App() {
 
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="*" element={<div>Not Found</div>} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<EarnAndSpendPage />} />
+        <Route path="/country-compare" element={<CountryComparePage />} />
+        <Route path="/country-statistics" element={<CountryStatisticPage />} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Route>
     </Routes>
   )
 }
